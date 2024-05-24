9ec5db66ba6477ec0b4ee6177ae4b86a0d5f8cbb
ANCB03 <ancabe1403@gmail.com>
2024-05-24 01:16:06 -0500

    Configuración de Mapa dinánmico.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+var map;
+
 function mapaCovid(urlData){
-    var map = new Datamap({
+  if (!map) {
+     map = new Datamap({
         element: document.getElementById('colombia'),
         scope: 'colombia',
         responsive: true, //if true, call `resize()` on the map object when it should adjust it's size
@@ -30,42 +33,34 @@ function mapaCovid(urlData){
           popupTemplate: function(geography, data) { //this function should just return a string
             //return '<div class="hoverinfo"><strong>' + geography.properties.name + '</strong></div>';
             //'<tr><td class="text-secondary">Curados: </td><td class="text-success">' +  data.curados + '</td></tr>',
-            console.log(geography.properties)
-            console.log(data)
-            if(data.muertos > 0 & data.curados > 0){
-              return ['<div class="card" style="padding:0;font-size:">',
+            //console.log(geography.properties)
+            //console.log(data)
+            
+             console.log(data)
+              if(urlData == 'col-data1.csv'){
+                return ['<div class="card" style="padding:0;font-size:">',
               '<h6 class="card-header bg-info">' +  geography.properties.name + '</h6>',
               '<div class="card-body"><table class="table text-left"><tbody>',
-              '<tr><td class="text-secondary">Diagnosticados</td><td class="text-danger">' +  data.infectados + '</td></tr>',
-              '<tr><td class="text-secondary">Muertes: </td><td class="text-dark">' +  data.muertos + '</td></tr>',
-              '<tr><td class="text-secondary">Curados: </td><td class="text-success">' +  data.curados + '</td></tr>',
+              '<tr><td class="text-secondary">Actividad Economica</td><td class="text-danger">' +  data.act + '</td></tr>',
               '</tbody></table></div></div>'].join('');
-            }else if(data.muertos > 0 & data.curados == 0){
-              return ['<div class="card" style="padding:0;font-size:">',
+              }else if(urlData == 'col-data2.csv'){
+                return ['<div class="card" style="padding:0;font-size:">',
               '<h6 class="card-header bg-info">' +  geography.properties.name + '</h6>',
               '<div class="card-body"><table class="table text-left"><tbody>',
-              '<tr><td class="text-secondary">Diagnosticados</td><td class="text-danger">' +  data.infectados + '</td></tr>',
-              '<tr><td class="text-secondary">Muertes: </td><td class="text-dark">' +  data.muertos + '</td></tr>',
-              //'<tr><td class="text-secondary">Curados: </td><td class="text-success">' +  data.curados + '</td></tr>',
+              '<tr><td class="text-secondary">2005</td><td class="text-danger">' +  data.a2005 + '</td></tr>',
+              '<tr><td class="text-secondary">2006: </td><td class="text-dark">' +  data.a2006 + '</td></tr>',
+              '<tr><td class="text-secondary">2007: </td><td class="text-success">' +  data.a2007 + '</td></tr>',
+              '<tr><td class="text-secondary">2008 </td><td class="text-success">' +  data.a2008 + '</td></tr>',
+              '<tr><td class="text-secondary">2009 </td><td class="text-success">' +  data.a2009 + '</td></tr>',
+              '<tr><td class="text-secondary">2010 </td><td class="text-success">' +  data.a2010 + '</td></tr>',
+              '<tr><td class="text-secondary">2011 </td><td class="text-success">' +  data.a2011 + '</td></tr>',
+              '<tr><td class="text-secondary">2012 </td><td class="text-success">' +  data.a2012 + '</td></tr>',
+              '<tr><td class="text-secondary">2013 </td><td class="text-success">' +  data.a2013 + '</td></tr>',
               '</tbody></table></div></div>'].join('');
-            }else if(data.muertos == 0 & data.curados > 0){
-              return ['<div class="card" style="padding:0;font-size:">',
-              '<h6 class="card-header bg-info">' +  geography.properties.name + '</h6>',
-              '<div class="card-body"><table class="table text-left"><tbody>',
-              '<tr><td class="text-secondary">Diagnosticados</td><td class="text-danger">' +  data.infectados + '</td></tr>',
-              //'<tr><td class="text-secondary">Muertes: </td><td class="text-dark">' +  data.muertos + '</td></tr>',
-              '<tr><td class="text-secondary">Curados: </td><td class="text-success">' +  data.curados + '</td></tr>',
-              '</tbody></table></div></div>'].join('');
-            }
-            else{
-              return ['<div class="card" style="padding:0;font-size:">',
-              '<h6 class="card-header bg-info">' +  geography.properties.name + '</h6>',
-              '<div class="card-body"><table class="table text-left"><tbody>',
-              '<tr><td class="text-secondary">Diagnosticados</td><td class="text-danger">' +  data.infectados + '</td></tr>',
-              //'<tr><td class="text-secondary">Muertes: </td><td class="text-dark">' +  data.muertos + '</td></tr>',
-              //'<tr><td class="text-secondary">Curados: </td><td class="text-success">' +  data.curados + '</td></tr>',
-              '</tbody></table></div></div>'].join('');
-            }
+              }
+                
+              
+            
           },
     
           popupOnHover: true, //disable the popup while hovering
@@ -88,6 +83,7 @@ function mapaCovid(urlData){
         }
     });
     
+
     window.addEventListener('resize', function(event){
         map.resize();
     });
@@ -99,14 +95,52 @@ function mapaCovid(urlData){
         }, 1000);
       //$('#exampleModalCenter').modal('show');
     });
+  }   else {
+    // Si el mapa ya está inicializado, actualizar los datos
+    d3.csv(urlData, function(newData) {
+      var dataObject = {};
+      // Procesa los nuevos datos y actualiza los valores correspondientes
+      newData.forEach(function(d) {
+        dataObject[d.id] = d;
+      });
+      // Actualiza el mapa con los nuevos datos
+      map.updateChoropleth(dataObject);
+      
+      // Actualiza el popupTemplate para reflejar los nuevos datos
+      map.options.geographyConfig.popupTemplate = function(geography, data) {
+        var html = '<div class="card" style="padding:0;font-size:">' +
+                   '<h6 class="card-header bg-info">' + geography.properties.name + '</h6>' +
+                   '<div class="card-body"><table class="table text-left"><tbody>';
+        
+        if (urlData === 'col-data1.csv') {
+          // Actualiza el popupTemplate para col-data1.csv
+          html += '<tr><td class="text-secondary">Actividad Economica</td><td class="text-danger">' + data.act + '</td></tr>';
+        } else if (urlData === 'col-data2.csv') {
+          // Actualiza el popupTemplate para col-data2.csv
+          html += '<tr><td class="text-secondary">2005</td><td class="text-danger">' + data.a2005 + '</td></tr>' +
+                  '<tr><td class="text-secondary">2006: </td><td class="text-dark">' + data.a2006 + '</td></tr>' +
+                  '<tr><td class="text-secondary">2007: </td><td class="text-success">' + data.a2007 + '</td></tr>' +
+                  '<tr><td class="text-secondary">2008 </td><td class="text-success">' + data.a2008 + '</td></tr>' +
+                  '<tr><td class="text-secondary">2009 </td><td class="text-success">' + data.a2009 + '</td></tr>' +
+                  '<tr><td class="text-secondary">2010 </td><td class="text-success">' + data.a2010 + '</td></tr>' +
+                  '<tr><td class="text-secondary">2011 </td><td class="text-success">' + data.a2011 + '</td></tr>' +
+                  '<tr><td class="text-secondary">2012 </td><td class="text-success">' + data.a2012 + '</td></tr>' +
+                  '<tr><td class="text-secondary">2013 </td><td class="text-success">' + data.a2013 + '</td></tr>';
+        }
+        
+        html += '</tbody></table></div></div>';
+        return html;
+      };
+    });
+  }
 }
 
 function actividadesEcon(){
-  mapaCovid('col-data.csv');
+  mapaCovid('col-data1.csv');
 }
 
 function PIBPreciosCorrientes(){
-  mapaCovid('col-data.csv');
+  mapaCovid('col-data2.csv');
 }
 
 function PIBPorcentaje(){
